1b52609dccf444a7acaae94e28df37573492b635
Andrej <gajduk.andrej@gmail.com>
2014-09-04 23:13:04 +0200

    Meanless commit

--- WirelessCooperation/src/main/CenterAntenaMain.java
+++ WirelessCooperation/src/main/CenterAntenaMain.java
@@ -5,6 +5,7 @@ import simulator.SimulationExperiment;
 public class CenterAntenaMain {
 	
 	public static void main(String[] args) throws Exception {
+		//ovoa nema am bash nikakva vrska
 		SimulationExperiment.main("-D 150000000 -S C -SA synch -T 19999 -N 50 -a 100 -EC minimal -alpha 4 -ni .5 -TL center -DF simple -F improved".split(" "));
 	}
 
